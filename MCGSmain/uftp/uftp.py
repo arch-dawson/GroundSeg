2264200a1c6b10a075b75c87ade66e2e4ac5fc4a
dbeattyspace <dawson@localhost.localdomain>
2016-05-28 23:16:26 -0600

    Adding some notes

--- MCGSmain/uftp/uftp.py
+++ MCGSmain/uftp/uftp.py
@@ -10,6 +10,24 @@ class uftpConnection:
         
 
     def initServ(self):
+        # Add -f and -F options
+        # Could just do a search to see if file exists?
+        # Could send really big file then close connection halfway through
+        # -i file_list.  Seperate files to be sent, one per line.
+        # -C tfmcc to dynamically determine transmission rate
+        # AES-256-CBC, SHA-1 hashing, autogen 512-bit RSA key:
+        # uftp -Y aes256-cbc -h sha1 [-k keyfile]
+        # -L logfile
+        # -S status_file for human-readable
+        # -g max_log_size (1-1024) MB
+        #  -n max_log_size (1-1000)
+        # Max combo of the two above will allow for more than 1TB of log files
+        # -m max_nak_count (1-10) probably change this if having lotsa naks
+        # -x Should we want to change detail in logs. (1-5), 5 has the most.
+        # Default for -x is 2
+        # -s robustness (10-50), default 20. number of times sending waiting for client
+        # -t time-to-live
+        # -r init_grtt[:min_grtt:max_grtt] CHANGE IF SENDING FAILS 
         pass
 
     def initClnt(self):
