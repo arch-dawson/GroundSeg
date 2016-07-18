73bce6c9fcf503f83b43ed4ee510cc4192a90731
dbeattyspace <dawson.beatty@gmail.com>
2016-07-18 15:18:12 -0600

    Made spaceTrack more general

--- spaceTrack.py
+++ spaceTrack.py
@@ -1,25 +1,31 @@
 import spacetrack.operators as op
 from spacetrack import SpaceTrackClient
+import argparse
+import sys
 
 # Space Track allows no more than 20 requests per minute
 
-import sys
+def main(noradID):
+    outFile = str(noradID) + '_3LE.txt'
 
-outFile = 'latestTLE.txt'
+    st = SpaceTrackClient('dawson.beatty@gmail.com', 'COSGCGroundSegment')
 
-noradID = 41474 # MINX
+    try:
+        st.authenticate() # Will raise an error if incorrect login
+    except:
+        print('Incorrect Login.')
+        sys.exit(0)
 
-st = SpaceTrackClient('dawson.beatty@gmail.com', 'COSGCGroundSegment')
+    data = st.tle_latest(iter_lines=True, norad_cat_id=noradID, limit=1, format='3le')
+    # Change format to whatever is easiest
 
-try:
-    st.authenticate() # Will raise an error if incorrect login
-except:
-    print('Incorrect Login.')
-    sys.exit(0)
+    with open(outFile, 'w+') as f:
+        for line in data:
+            f.write(line)
 
-data = st.tle_latest(iter_lines=True, norad_cat_id=noradID, limit=1, format='tle')
-# Change format to whatever is easiest
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Requests most recent 3LE data from spacetrack.org')
+    parser.add_argument('-id', action="store", dest="noradID", type=str, default=41474) #41474 is MINX
+    args = parser.parse_args()
 
-with open(outFile, 'w+') as f:
-    for line in data:
-        f.write(line)
+    main(args.noradID)
